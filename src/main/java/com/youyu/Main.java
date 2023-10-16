36d0a9629104165e5c12c9850d34ffc63e19c498
youyu-yaochen <148050316+youyu-yaochen@users.noreply.github.com>
2023-10-16 21:30:56 +0800

    pull commit

--- src/main/java/com/youyu/Main.java
+++ src/main/java/com/youyu/Main.java
@@ -10,5 +10,6 @@ public class Main {
         System.out.println("hot-fix test");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("pull test");
     }
 }
