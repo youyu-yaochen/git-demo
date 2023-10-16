f820108fd5671811e6db8fd04946c3bae406c877
yaochen <1564140883@qq.com>
2023-10-16 21:24:24 +0800

    push commit

--- src/main/java/com/youyu/Main.java
+++ src/main/java/com/youyu/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
